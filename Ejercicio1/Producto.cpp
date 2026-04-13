5e0521a2f6026541cd31509ef1cedcf7c5b348fe
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-04-13 15:23:32 +0200

    Implementar Producto

--- Ejercicio1/Producto.cpp
+++ Ejercicio1/Producto.cpp
@@ -1,3 +1,43 @@
-//
-// Created by gonza on 10/04/2026.
-//
+#include "Producto.h"
+
+Producto::Producto() : codigoProducto(""), nombreProducto(""), categoria(""), precioUnitario(0.0), stock(0)
+{}
+
+Producto::Producto(const std::string &codigo, const std::string &nombre, const std::string &categoria,
+    double precioUnitario, int stock)
+        : codigoProducto(codigo), nombreProducto(nombre), categoria(categoria), precioUnitario(precioUnitario), stock(stock)
+{}
+
+const std::string Producto::getCodigoProducto() const { return codigoProducto; }
+
+const std::string Producto::getNombreProducto() const { return nombreProducto; }
+
+const std::string Producto::getCategoria() const { return categoria; }
+
+const double Producto::getPrecioUnitario() const { return precioUnitario; }
+
+int Producto::getStock() const { return stock; }
+
+void Producto::setNombreProducto(const std::string &InNombre) { nombreProducto = InNombre; }
+
+void Producto::setCategoria(const std::string &InCategoria) { categoria = InCategoria; }
+
+void Producto::setPrecioUnitario(double InPrecioUnitario) { precioUnitario = InPrecioUnitario; }
+
+void Producto::setStock(int InStock) { stock = InStock; }
+
+bool Producto::operator<(const Producto &otro) const {
+    return codigoProducto < otro.codigoProducto;
+}
+
+bool Producto::operator>(const Producto &otro) const {
+    return codigoProducto > otro.codigoProducto;
+}
+
+bool Producto::operator==(const Producto &otro) const {
+    return codigoProducto == otro.codigoProducto;
+}
+
+bool Producto::operator!=(const Producto &otro) const {
+    return !(*this == otro);
+}
--- Ejercicio1/Producto.h
+++ Ejercicio1/Producto.h
@@ -9,4 +9,30 @@ private:
     std::string categoria;
     double precioUnitario;
     int stock;
+
+public:
+    Producto();
+
+    Producto(const std::string& codigo,
+        const std::string& nombre,
+        const std::string& categoria,
+        double precioUnitario,
+        int stock);
+
+    const std::string getCodigoProducto() const;
+    const std::string getNombreProducto() const;
+    const std::string getCategoria() const;
+    const double getPrecioUnitario() const;
+    int getStock() const;
+
+    void setNombreProducto(const std::string& InNombre);
+    void setCategoria(const std::string& InCategoria);
+    void setPrecioUnitario(double InPrecioUnitario);
+    void setStock(int InStock);
+
+    // Operadores de comparación (para el ABB)
+    bool operator<(const Producto& otro) const;
+    bool operator>(const Producto& otro) const;
+    bool operator==(const Producto& otro) const;
+    bool operator!=(const Producto& otro) const;
 };
