65506f9cf20e0a1c4c9f400b2cd6b6480a247b19
Gonzalo Blanch Dominguez <gonzaloblanchdev@gmail.com>
2026-04-13 15:14:41 +0200

    Implementar nodo

--- Ejercicio1/Nodo.h
+++ Ejercicio1/Nodo.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#include <memory>
+
 template<typename T>
 class NodoArbol {
 private:
+    T data;
+    std::shared_ptr<NodoArbol<T>> izq_;
+    std::shared_ptr<NodoArbol<T>> der_;
 
+public:
+    NodoArbol(cont T& dato) : data(dato), izq_(nullptr), der_(nullptr) {}
+
+    const T& getData() const { return data; }
+    std::shared_ptr<NodoArbol<T>> getIzq() { return izq_; }
+    std::shared_ptr<NodoArbol<T>> getDer() { return der_; }
+
+    void setData(const T& dato) { data = dato; }
+    void setIzq(std::shared_ptr<NodoArbol<T>> izq) { izq_ = izq; }
+    void setDer(std::shared_ptr<NodoArbol<T>> der) { der_ = der; }
 };
