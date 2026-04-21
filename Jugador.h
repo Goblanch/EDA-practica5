44d28987adf17d2ad7a1fcd8b31f2d4c41543b2a
ddaniwey <ddaniwey@gmail.com>
2026-04-21 14:00:10 +0200

    Jugador acabado

--- Jugador.h
+++ Jugador.h
@@ -1,8 +1,61 @@
-//
-// Created by gonza on 21/04/2026.
-//
+#pragma once
+#include <iostream>
 
-#ifndef PRACTICA5_JUGADOR_H
-#define PRACTICA5_JUGADOR_H
+class Jugador
+{
+private:
+    std::string id;
+    std::string nickname;
+    std::string equipo;
+    int nivel; // mayor a 0
+    double puntuacion_previa;
+    bool baneado;
 
-#endif //PRACTICA5_JUGADOR_H
+public:
+    Jugador(std::string nick, std::string equip, int niv, double punt)
+        : nickname(nick), equipo(equip), nivel(0), puntuacion_previa(0)
+    {
+        setId(nick, equip);
+        setNivel(niv);
+        setPuntuacion(punt);
+    }
+    void setNivel(const int niv)
+    {
+        if (niv > 0)
+        {
+            nivel = niv;
+            return;
+        }
+            nivel = 1;
+            std::cout<<"El nivel tiene que ser mayor a 0"<<std::endl;
+    }
+    int getNivel(){ return nivel; }
+    double getPuntuacion(){ return puntuacion_previa; }
+    std::string getId(){ return id; }
+    std::string getNickname(){ return nickname; }
+
+    void setPuntuacion(double punt)
+    {
+        if (punt >= 5000)
+        {
+            std::cout<<"La puntuación tiene que ser menor o igual a 5000"<<std::endl;
+            return;
+        }
+        puntuacion_previa = punt;
+        comprobarBaneo();
+
+    }
+
+    void setId(std::string nick, std::string equip)
+    {
+        nickname = nick;
+        equipo = equip;
+        id = nick + "-" + equip;
+    }
+
+    void comprobarBaneo()
+    {
+        baneado = (puntuacion_previa < 0);
+    }
+
+};
